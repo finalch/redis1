73081455a27c60991324ea300e725e91b2ea7f6b
finalch <finalch@163.com>
2021-03-22 18:23:44 +0800

    init

---
+++ deps/hiredis/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /Users/liufan/Desktop/projects/redis/deps/hiredis
+# Build directory: /Users/liufan/Desktop/projects/redis/deps/hiredis
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(hiredis-test "/Users/liufan/Desktop/projects/redis/deps/hiredis/test.sh")
+set_tests_properties(hiredis-test PROPERTIES  _BACKTRACE_TRIPLES "/Users/liufan/Desktop/projects/redis/deps/hiredis/CMakeLists.txt;83;ADD_TEST;/Users/liufan/Desktop/projects/redis/deps/hiredis/CMakeLists.txt;0;")
